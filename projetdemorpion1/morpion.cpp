6ce26c9b7812be8910fa604dee4cf9ed60a5c46c
AleksDominik <aleksnougbele@gmail.com>
2017-03-13 12:26:33 +0000

    derniere idee
    creation de la classe jeudemorpion et declaration des methodes

--- projetdemorpion1/morpion.cpp
+++ projetdemorpion1/morpion.cpp
@@ -1,10 +1,22 @@
 #include "morpion.h"
 
-
-void kase::colorChanged() {
- PropertyChanges{
-    target:fondM.'coodonee'
- color=m_color};
+//methode de kase
+kase::kase(QObject *parent,string Coo):QObject(parent )
+ {
+    m_color='000000';
+    coordonnee=coo;
 }
+kase:: getcoord(const kase &D)
+{   colorchanged();
+    return coordonnee
+}
+
 kase::kase(string a):coordonnee(a)
 {}
+//methode de joueur
+
+
+
+
+//methode de partie
+
--- projetdemorpion1/morpion.h
+++ projetdemorpion1/morpion.h
@@ -2,50 +2,33 @@
 #define MORPION_H
 #include <QObject>
 
-class kase : public QObject
+class jeumorpion : public QObject
 {
     Q_OBJECT
+
   public:
-
-
-Q_INVOKABLE void joueur1();
+Q_INVOKABLE void getcolor();
+Q_INVOKABLE void getindex();
 Q_INVOKABLE void joueur2();
-void textChanged(const QString &newText);
-kase(string a)  ;
+QString getcoord(kase const&  D);
+explicit kase (QObject *parent = 0);
+
 signals:
 colorChanged();
-private:
- QColor  m_color='ff0000';
- QString coordonnee ;
-};
-class joueur: public QObject
- {
-    Q_OBJECT
-public:
-joueur( QColor b);
- kase(string a)  ;
- signals:
+
+public slots:
+void ajouer();
 
 private:
+QString nomdelapartie;
 int index=0;
-};
-class partie : public QObject
-       {
-
-    Q_OBJECT
-  public:
+QColor  couleurdujoeur1='ff0000';
+QColor  couleurdujoeur2='ff0000';
+QString coordonnee ;
 
 
-Q_INVOKABLE void increment();
-void textChanged(const QString &newText);
-kase(string a)  ;
-signals:
-colorChanged();
-private:
- QColor  m_color='ff0000';
- QString coordonnee ;
 
-};
+}
 
 
 
